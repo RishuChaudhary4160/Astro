841e71489b9840c267ab7f23c05d3ee324e7942d
rishu <rishuchaudhary417@gmail.com>
2025-07-04 15:48:37 +0530

    fixes bug

---
+++ src/components/FloatingButtons.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FloatingButtons = () => {
+  return (
+    <div
+      className="col-xs-12 col-sm-12 col-md-12 col-lg-12 fixed"
+      style={{ zIndex: 99999 }}
+    >
+      <ul className="end">
+        <li className="phone ph-fx">
+          <a
+            href="tel:+919510613656"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-attr="phone"
+            data-toggle="tooltip"
+            title="Contact"
+          >
+            <i className="fa fa-phone element2"></i>
+          </a>
+        </li>
+        <li className="what1 wh-fx">
+          <a
+            href="https://api.whatsapp.com/send?phone=919510613656&text=I'd like to know more about your astrology services. Please contact me"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-attr="whatsapp_contact"
+            data-toggle="tooltip"
+            title="Whatsapp"
+          >
+            <i className="fa fa-whatsapp element2"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default FloatingButtons;
--- src/components/MarriageProblemSolution.jsx
+++ src/components/MarriageProblemSolution.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Breadcrumb from "../components/Breadcrumb";
 
 const MarriageProblemSolution = () => {
+  useEffect(() => {
+    document.title = "Marriage Problem Solution | Om Astro Service";
+  }, []);
+
   return (
-    <div className="marriage-problem-solution">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1>Marriage Problem Solution</h1>
-            <p>
-              Facing issues in your married life? Our experienced astrologers
-              provide effective solutions for all marriage-related problems.
-            </p>
-            <h3>We Help With:</h3>
-            <ul>
-              <li>Marital discord resolution</li>
-              <li>Communication improvement</li>
-              <li>Trust building</li>
-              <li>Financial harmony</li>
-              <li>In-law relationship issues</li>
-            </ul>
+    <div>
+      <Breadcrumb title="Marriage Problem Solution" />
+      <div className="ast_about_wrapper ast_toppadder70 ast_bottompadder70">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+              <div className="ast_heading">
+                <h2>
+                  Marriage Problem <span>Solution</span>
+                </h2>
+                <p>Astrological solutions for marriage-related challenges</p>
+              </div>
+            </div>
+            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+              <div className="ast_about_info">
+                <p>
+                  When navigating hurdles on the path to marriage, astrology
+                  offers solutions. Whether due to planetary changes or chart
+                  discrepancies, it can address pre- and post-marriage issues.
+                  While some react aggressively, a composed approach turns to
+                  astrology to alleviate emotional distress caused by a
+                  partner's actions.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
